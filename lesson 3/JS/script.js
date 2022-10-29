56b6e29f67f38a816d3d21a1302509dbf0e23e7b
Andrew Tatarzhytski <startforweb@gmail.com>
2022-10-29 19:30:12 +0300

    Урок 3

---
+++ lesson 3/JS/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(4 / 0);
+console.log('string' * 0); // NaN
+
+const person = "Andrew";
+
+const bool = true;
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "john",
+    age: 25,
+    isMaried: true
+};
+
+// console.log(obj.name);
+
+console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.png', {}, []];
+console.log(arr[1]);
+
