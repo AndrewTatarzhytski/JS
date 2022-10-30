c77177e8afbde2d982e3b87f978a8f8fb97768ee
Andrew Tatarzhytski <startforweb@gmail.com>
2022-10-30 20:47:32 +0300

    Урок 6 Операторы в JS

---
+++ lesson 6/JS/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(10 % 2);
+
+console.log(2 + 2 * 2 != '6'); //false
+
+
+const isChecked = true,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
