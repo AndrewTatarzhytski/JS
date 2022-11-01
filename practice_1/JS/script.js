b68707c5d7728e9eca8b9110597e457f0b543b0a
AndrewTatarzhytski <startforweb@gmail.com>
2022-11-01 16:06:06 +0300

    Создал объект personalMovieDB

--- practice_1/JS/script.js
+++ practice_1/JS/script.js
@@ -1,4 +1,12 @@
 "use strict";
 
-let numberOfFilms = prompt("Какое колличество фильмов вы посмотрели?", "");
+let numberOfFilms = +prompt("Какое колличество фильмов вы посмотрели?", "");
 console.log(numberOfFilms);
+
+const personalMovieDB={
+    count: numberOfFilms,
+    movies:{},
+    actor:{},
+    genres:[],
+    privat:false
+};
