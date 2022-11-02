6cb02240b529f6bae007253bfc237474757868c8
AndrewTatarzhytski <startforweb@gmail.com>
2022-11-02 13:14:08 +0300

    Практика ч2, задание 1

--- practice_2/JS/script.js
+++ practice_2/JS/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-let numberOfFilms = +prompt("Какое колличество фильмов вы посмотрели?", "");
-console.log(numberOfFilms);
+ let numberOfFilms = +prompt("Какое колличество фильмов вы посмотрели?", "");
+
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,12 +11,15 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов', ''),
+            b = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB);
