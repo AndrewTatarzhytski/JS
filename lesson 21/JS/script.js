1e4da4210d8689b12a618b720983f0af1cf1bad0
AndrewTatarzhytski <startforweb@gmail.com>
2022-11-09 16:53:01 +0300

    урок 21, видео 26, Действия с элементами на странице

--- lesson 21/JS/script.js
+++ lesson 21/JS/script.js
@@ -1,11 +1,11 @@
-'use strict'; // урок 21, видео 26, Действия с элементами на странице -> продолжиьт с 23:34
+'use strict'; // урок 21, видео 26, Действия с элементами на странице
 
 const box = document.getElementById('box'),
     btns = document.getElementsByTagName('button'),
     circles = document.getElementsByClassName('circle'),
-    hearts = document.querySelectorAll('.heart'),
-    oneHeart = document.querySelector('div'),
-    wrapper = document.querySelector('.wrapper');
+    wrapper = document.querySelector('.wrapper'),
+    hearts = wrapper.querySelectorAll('.heart'),
+    oneHeart = wrapper.querySelector('div');
 
 
 // box.style.backgroundColor = 'blue';
@@ -30,17 +30,29 @@ const div=document.createElement('div');
 div.classList.add('black');
 
 // document.querySelector('.wrapper').append(div); // помещает элемент в конец списка элементов
-// wrapper.appendChild(div);
+wrapper.appendChild(div);
 
 
 // wrapper.prepend(div); // помещает элемент в начало списка элементов
 
 // hearts[1].before(div); // помещает элемент вперед
-wrapper.insertBefore(div, hearts[1]);
+// wrapper.insertBefore(div, hearts[1]);
+
 
 // hearts[1].after(div); // помещает элемент назад
 
 // circles[0].remove(); // удаление элемента
+// wrapper.removeChild(hearts[1]); // удаление элемента
 
-// hearts[0].replaceWith(circles[0]); // заменяет элемент кузанным элементом
+// hearts[0].replaceWith(circles[0]); // заменяет элемент укзанным элементом
+// wrapper.replaceChild(circles[0], hearts[0]); // заменяет элемент укзанным элементом
+
+div.innerHTML = '<h1>Hello world</h1>'; // вставка другого содержимого в элемент на странице
+
+// div.textContent = 'Hello'; // вставка текста в элемент на странице
+// div.insertAdjacentHTML('beforebegin', '<h2>Hello</h2>'); // вставка HTML-кода перед элементом
+// div.insertAdjacentHTML('afterbegin', '<h2>Hello</h2>'); // вставка HTML-кода в начало элемента
+// div.insertAdjacentHTML('beforeend', '<h2>Hello</h2>'); // вставка HTML-кода в конец элемента
+// div.insertAdjacentHTML('afterend', '<h2>Hello</h2>'); // вставка HTML-кода после элемента
+
 
