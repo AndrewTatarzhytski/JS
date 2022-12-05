732a447b1b71c71b3ca99b0fb6049ec1384fc5ca
Andrew Tatarzhytski <startforweb@gmail.com>
2022-12-05 21:47:40 +0300

    урок 31 события и их обработчики

---
+++ lesson 22/JS/script.js
@@ -0,0 +1,36 @@
+'use strict'; // урок 31 события и их обработчики
+
+const btns = document.querySelectorAll('button'),
+    overlay = document.querySelector('.overlay');
+// btn.onclick = function () {
+//     alert('click');
+// };
+
+// btn.onclick = function () {
+//     alert('second click');
+// };
+
+let i = 0;
+const deleteElement = (e) => {
+    console.log(e.target);
+    console.log(e.type);
+    // i++;
+    // if (i==1){
+    //     btn.removeEventListener('click', deleteElement);
+    // }
+};
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement, {once:true});
+});
+
+const link = document.querySelector('a');
+
+link.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    console.log(event.target);
+});
