7a092d1b00f7defcbdb380cbd1eb47dc627a95a8
AndrewTatarzhytski <startforweb@gmail.com>
2022-12-07 12:17:26 +0300

    урок 27 видео  038 создаем табы в новом проекте

---
+++ lesson 27 ( создаем табы в новом проекте)/JS/script.js
@@ -0,0 +1,40 @@
+'use strict'; // 038 создаем табы в новом проекте
+
+window.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) { // i = 0 значение по умолчанию
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent(); // прячет содержимое табов
+    showTabContent(); // показывает содержимое табов
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
