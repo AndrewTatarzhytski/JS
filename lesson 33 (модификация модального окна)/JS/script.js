bd72d103d3fa336c4a7456f38b39b4a007423025
AndrewTatarzhytski <startforweb@gmail.com>
2022-12-15 10:08:09 +0300

    урок 33 видео 044 Модификации модального окна

---
+++ lesson 33 (модификация модального окна)/JS/script.js
@@ -0,0 +1,148 @@
+'use strict'; // 044 Модификации модального окна
+
+window.addEventListener('DOMContentLoaded', () => {
+    // ТАБЫ
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) { // i = 0 значение по умолчанию
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent(); // прячет содержимое табов
+    showTabContent(); // показывает содержимое табов
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+    // ТАЙМЕР
+
+    const deadLine = '2022-12-31'; // дата окончания
+
+    function getTimeRemaining(endTime) { // функция получения времени обратного отсчета
+        const t = Date.parse(endTime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)), // % - остаток от деления
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num) { // подставляем ноль если значение меньше 10
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) { // функция таймера
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock(); // объявляем функцию чтобы убрать данные таймера из верстки
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadLine);
+
+    // МОДАЛЬНОЕ ОКНО
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalBtnClose = document.querySelector('[data-close]');
+
+    function openModal() {
+        // modal.classList.add('show');
+        // modal.classList.remove('hide');
+        modal.classList.toggle('show');
+        document.body.style.overflow = 'hidden'; // блокируем прокрутку страницы при открытом модальном окне
+        clearInterval(modalTimerId); // запрещает открывать окно, если оно уже было открыто
+    }
+
+    modalTrigger.forEach(btn => { // открытие модального окна
+        btn.addEventListener('click', openModal);
+    });
+
+    function closeModal() { // функция закрытия модального окна
+        // modal.classList.add('hide');
+        // modal.classList.remove('show');
+        modal.classList.toggle('show');
+        document.body.style.overflow = ''; // снимаем блокировку при закрытии окна
+    }
+
+    modalBtnClose.addEventListener('click', closeModal); // закрывает окно
+
+    modal.addEventListener('click', (e) => { // закрывает окно при клике на подложку
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => { // закрытие модального окна по нажатию клавиши ESC
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll(){ // функция открытия модального окна при прокрутке до конца страницы
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll); // если окно было открыто 1 раз после прокрутки до конца страницы, то оно больше не откроется
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll); // открывает модальное окно при прокрутке до конца страницы
+        
+    
+
+});
